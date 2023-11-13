5c6f7e10f5b833faf95a523d1feb82beda74bbcf
Nikolai Petrishchev <nik.petrishchev@gmail.com>
2023-11-13 20:21:36 +0300

    Add template for file structure

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,6 +21,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'core.apps.CoreConfig',
+    'users.apps.Users.Config',
 ]
 
 MIDDLEWARE = [
