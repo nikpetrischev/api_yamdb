4cd0a5ef012d984eb6ca97431d755ff69bc256d5
Denzol255 <den-zoloto@mail.ru>
2023-11-14 19:36:01 +0300

    Add base review structure

---
+++ api_yamdb/api/v1/serializers.py
@@ -0,0 +1,24 @@
+from reviews.models import Comment, Review
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+        read_only_fields = ('title',)
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'text', 'author', 'pub_date')
+        read_only_fields = (
+            'title',
+            'review',
+        )
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+
+class Review(models.Model):
+    text = models.TextField(verbose_name='Текст отзыва')
+    # author = models.ForeignKey(
+    #     User,
+    #     on_delete=models.CASCADE,
+    #     related_name='reviews',
+    #     verbose_name='Автор отзыва',
+    # )
+    score = models.IntegerField()
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата и время отзыва',
+    )
+    # title = models.ForeignKey(
+    #     Title,
+    #     on_delete=models.CASCADE,
+    #     related_name='reviews',
+    #     verbose_name='Произведение',
+    # )
+
+    class Meta:
+        verbose_name = "отзыв"
+        verbose_name_plural = "Отзыва"
+
+
+class Comment(models.Model):
+    text = models.TextField(verbose_name='Текст комментария')
+    # author = models.ForeignKey(
+    #     User,
+    #     on_delete=models.CASCADE,
+    #     related_name='reviews',
+    #     verbose_name='Автор комментария',
+    # )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата и время комментария',
+    )
+    # title = models.ForeignKey(
+    #     Title,
+    #     on_delete=models.CASCADE,
+    #     related_name='comments',
+    #     verbose_name='Произведение',
+    # )
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Отзыв',
+    )
+
+    class Meta:
+        verbose_name = "комментарий"
+        verbose_name_plural = "Комментария"
